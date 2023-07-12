10127d7592f98d9d8eab99b477ba6a6ea06cbc18
NayZ.Sodium <null>
2023-07-12 15:18:24 +0200

    Adding Dockerfile

--- app.test.js
+++ app.test.js
@@ -5,3 +5,7 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
+
+test('addition returns number added', () => {
+    const result = add
+})
